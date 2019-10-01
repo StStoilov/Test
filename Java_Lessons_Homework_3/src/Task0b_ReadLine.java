7c3d39e632606fab98ae55d95beac67878253cc3
Stefan Stoilov <stefan.stoilov.2016@gmail.com>
2019-10-01 20:44:12 +0300

    Deleted

--- Java_Lessons_Homework_3/src/Task0b_ReadLine.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Task0b_ReadLine {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String line = sc.nextLine();
-        System.out.println(line);
-    }
-}
